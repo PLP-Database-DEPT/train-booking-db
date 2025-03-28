8b93910e5de917af1d2aa627b45cc7b2e20992c3
MaingiKiio <stanleykiio57@gmail.com>
2025-03-28 14:47:12 +0300

    added command for adding carriage_price TABLE

--- db.sql
+++ db.sql
@@ -1,1 +1,10 @@
+-- trainbookingDb
 
+-- carriage_price table | From bash terminal
+CREATE TABLE carriage_price(
+    -> schedule_id INT,
+    -> carriage_class_id INT,
+    -> price DECIMAL(10,2),
+    -> FOREIGN KEY(schedule_id) REFERENCES schedule(id),
+    -> FOREIGN KEY(carriage_class_id) REFERENCES carriage_class(id)
+    -> );
