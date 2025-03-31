44980aba110c0e629dddbad760d57909ebd3ed5a
Kanti Sarah <169155874+S-kanti@users.noreply.github.com>
2025-03-31 11:58:00 +0100

    Creating booking_status table (#23)
    * Creating booking_status table
    
    * Update booking_status table.
    
    ---------
    
    Co-authored-by: kantisarah <kantisarah@gmail.com>
    Co-authored-by: PLP-Database-DEPT <zablon@powerlearnprojectafrica.org>

---
+++ booking_status_table.sql
@@ -0,0 +1,6 @@
+-- booking_status table --
+USE trainbookingDB;
+CREATE TABLE booking_status(
+id int primary key ,
+name varchar(100) 
+);
--- db.sql
+++ db.sql
@@ -1,10 +1,5 @@
 CREATE DATABASE trainbookingDB;
 
-
-
-
-
-
 CREATE TABLE booking (
     id INT NOT NULL PRIMARY KEY,
     carriage_class_id INT,
@@ -27,11 +22,10 @@ CREATE TABLE booking (
 	FOREIGN KEY (ticket_class_id) REFERENCES carriage_class(id),
 );
 
--- carriage_price table
 CREATE TABLE carriage_price(
-    -> schedule_id INT,
-    -> carriage_class_id INT,
-    -> price DECIMAL(10,2),
-    -> FOREIGN KEY(schedule_id) REFERENCES schedule(id),
-    -> FOREIGN KEY(carriage_class_id) REFERENCES carriage_class(id)
-    -> );
+     schedule_id INT,
+     carriage_class_id INT,
+     price DECIMAL(10,2),
+     FOREIGN KEY(schedule_id) REFERENCES schedule(id),
+     FOREIGN KEY(carriage_class_id) REFERENCES carriage_class(id)
+    );
